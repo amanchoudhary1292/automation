ad8de383bf69000ffa393eb7cf29966a457379ac
a13400510 <a13400510@192.168.43.193>
2021-06-04 23:49:58 +0530

    First Commit - Automation

---
+++ WPAADL/src/test/java/AADL/Flow1Test.java
@@ -0,0 +1,17 @@
+package AADL;
+
+import org.testng.annotations.Test;
+
+public class Flow1Test {
+	
+	@Test
+	public void flow1() {
+		System.out.println("test case 1 passed");
+	}
+	
+	@Test
+	public void flow2() {
+		System.out.println("test case 2 passed");
+	}
+
+}
---
+++ WPAADL/src/test/java/AADL/Flow2Test.java
@@ -0,0 +1,17 @@
+package AADL;
+
+import org.testng.annotations.Test;
+
+public class Flow2Test {
+	
+	@Test
+	public void flow3() {
+		System.out.println("test case 4 passed");
+	}
+	
+	@Test
+	public void flow4() {
+		System.out.println("test case 4 passed");
+	}
+
+}
